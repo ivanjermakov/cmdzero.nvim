46e3f08e8405fdb8f3c5c83864952217c6120282
ivanjermakov <ivanjermakov1@gmail.com>
2024-01-27 01:43:26 +0100

    Map `<esc>` to send `msg_clear` another way

--- lua/cmdzero/init.lua
+++ lua/cmdzero/init.lua
@@ -1,12 +1,17 @@
+local config = require("cmdzero.config")
+
 local M = {}
 
 function M.setup()
     require("cmdzero.config").setup()
     require("cmdzero.queue").setup()
     require("cmdzero.ui").setup()
-    vim.keymap.set("n", "<esc>", function()
+    -- on_key is used instead of normal vim.keymap.set because it interferes with another esc mappings
+    -- (closing vim.lsp.buf.hover no longer works)
+    vim.on_key(function(char)
+        if char:byte() ~= 128 then return end
         require("cmdzero.queue").queue({ event = "msg_clear" })
-    end)
+    end, config.ns)
 end
 
 return M
